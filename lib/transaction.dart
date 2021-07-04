265adad3279d2c48197bcce061b80ccfc7a213a6
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-04 02:06:51 -0400

    added Transaction as a class; displaying a transaction as a card

---
+++ lib/transaction.dart
@@ -0,0 +1,13 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction({
+    required this.id,
+    required this.amount,
+    required this.date,
+    required this.title,
+  });
+}
