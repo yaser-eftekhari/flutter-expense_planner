9d94b450327e5a3ce080a845cb99be7d9842dfc4
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-05 00:52:26 -0400

    added number keyboard for the amount

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -22,6 +22,7 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
             ),
             TextButton(
               onPressed: () {
