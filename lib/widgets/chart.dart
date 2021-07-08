7b7c6ab102e8398132fa5ff3fbfce36e50495b00
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-07 21:27:52 -0400

    added styling to the chart and chart bar

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -41,13 +41,20 @@ class Chart extends StatelessWidget {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactionValues.map((e) {
-          return ChartBar(
-              label: (e['day'] as String),
-              spendingAmount: (e['amount'] as double),
-              spendingRatio: totalSpending == 0 ? 0 : (e['amount'] as double)/totalSpending);
-        }).toList(),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactionValues.map((e) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                  label: (e['day'] as String),
+                  spendingAmount: (e['amount'] as double),
+                  spendingRatio: totalSpending == 0 ? 0 : (e['amount'] as double)/totalSpending),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
