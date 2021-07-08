fa48819b340c7ee99cdd9e9818cf6f0e72d976cd
Yaser Eftekhari <yaser.eftekhari@gmail.com>
2021-07-07 21:12:14 -0400

    adding bars to the chart; needs more styling

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import '../model/transaction.dart';
+import 'chart_bar.dart';
 
 class Chart extends StatelessWidget {
   final List<Transaction> recentTransactions;
 
   Chart(this.recentTransactions);
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (previousValue, element) {
+      return previousValue + (element['amount'] as double);
+    });
+  }
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
@@ -15,14 +22,17 @@ class Chart extends StatelessWidget {
       );
 
       double sum = 0;
-      for(var i = 0; i < recentTransactions.length; i++) {
-        if(recentTransactions[i].date.day == weekDay.day &&
-        recentTransactions[i].date.month == weekDay.month &&
-        recentTransactions[i].date.year == weekDay.year)
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year)
           sum += recentTransactions[i].amount;
       }
 
-      return {'day': DateFormat.E().format(weekDay), 'amount': sum};
+      return {
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
+        'amount': sum
+      };
     });
   }
 
@@ -32,7 +42,12 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((e) {
+          return ChartBar(
+              label: (e['day'] as String),
+              spendingAmount: (e['amount'] as double),
+              spendingRatio: (e['amount'] as double)/totalSpending);
+        }).toList(),
       ),
     );
   }
